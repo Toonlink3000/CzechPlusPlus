1f1ce5a73e79a6f01b7156768d7ee7e3eb320050
Toonlink3000 <dannyprague@seznam.cz>
2024-02-03 16:25:22 +0100

    Add project files.

---
+++ czechplusplus/czpp.h
@@ -0,0 +1,44 @@
+#ifndef _I_CZPP
+#define _I_CZPP
+
+#include <cstdio>
+#include <string>
+#include <array>
+
+#define pokud if
+#define jinak else
+#define pro for
+#define dokudje while
+
+#define pravda true
+#define nepravda false
+
+#define tøída class
+#define veøejné public
+#define soukromé private
+#define chránìné protected
+
+#define pole std::array
+
+#define rovno ==
+#define nerovno !=
+#define vìtšínež >
+#define menšínež <
+
+#define konst const
+#define loghod bool
+#define celé int
+#define plovoucí float
+#define dvojitìpøesné double
+#define písmeno char
+#define øetìzec std::string
+#define neznaménkový unsigned
+#define znaménkový signed
+
+#define hlavní main
+#define výstupf printf
+#define vstupf scanf_s
+#define vložit #include
+
+#endif
+
---
+++ czechplusplus/main.cpp
@@ -0,0 +1,20 @@
+#include "czpp.h"
+
+tøída test 
+{
+veøejné:
+	celé trest;
+	øetìzec test;
+};
+
+celé hlavní(celé poèet, konst písmeno* argumenty) 
+{
+	výstupf("Ahoj svìte\n");
+
+	loghod cyklus = pravda;
+
+	dokudje(cyklus rovno pravda)
+	{
+		výstupf("cyklus\n");
+	}
+}
